e6318ad58e1c134c17874c8dbdb700d3c335e60b
213489628 <yoavst22@gmail.com>
2020-12-29 15:16:52 +0200

    Update receiver.py

---
+++ coder.py
@@ -0,0 +1,46 @@
+import os
+import pathlib
+import sys
+
+
+def char_to_bin(c):
+    return "{:08b}".format(ord(c))
+
+
+def str_to_bin(str):
+    arr = []
+    for c in str:
+        arr.append(char_to_bin(c))
+    return arr
+
+
+def file_to_bin(file_name):
+    inp = open(file_name, 'r')
+    lines_bin = []
+    # for line in inp:
+    #     lines_bin += str_to_bin(line)
+    lines_bin += str_to_bin(inp.read())
+    return lines_bin
+
+
+def bin_to_char(bin):
+    return chr(int(bin, 2))
+
+
+def bin_to_str(arr):
+    str = "***\n"
+    for bin in arr:
+        str += bin_to_char(bin)
+    return str + "\n***"
+
+
+def bin_from_dir():
+    path = pathlib.Path().absolute()
+    files = os.listdir(path)
+    for f in files:
+        if '.txt' in f:
+            return file_to_bin(f)
+
+arr = bin_from_dir()
+print(arr)
+print(bin_to_str(arr))
