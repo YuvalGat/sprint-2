36cbc663bbbbc9bcc1870946988fc6533124a1b9
AmitWaksman <75178332+AmitWaksman@users.noreply.github.com>
2020-12-29 21:33:31 +0200

    ard update 2.0

--- arduino comunicate.py
+++ arduino comunicate.py
@@ -1,12 +1,16 @@
 import serial
 import time
+import coder
 
-WAIT_TIME = 0.4
+WAIT_TIME = 0.1
 
 
 def open_arduino(char_lst):
+    if len(char_lst) % 2 == 1:
+        char_lst.append('\n')
+
     ser = serial.Serial('COM6', 9600)  # opens communication with arduino
-    time.sleep(2)
+    time.sleep(1.6)
 
     for char in char_lst:
         ser.flush()
@@ -17,5 +21,5 @@ def open_arduino(char_lst):
 
 
 if __name__ == '__main__':
-    char_lst = ['a', 'b', 'c', 'd','e','f','g','h']
+    char_lst = coder.coder()
     open_arduino(char_lst)
